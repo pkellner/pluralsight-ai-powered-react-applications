9912ba7ca1cca284d2efc05f47bf8b01da67421b
pkellner <peter@peterkellner.net>
2025-02-05 15:59:34 -0800

    update

---
+++ m2-exchange-rate-chat/clip-02/server.js
@@ -0,0 +1,8 @@
+import { generateText } from "ai";
+import { openai } from "@ai-sdk/openai";
+
+generateText({
+  model: openai("gpt-4"),
+  prompt: "What is more valuable, 50 dollars or 50 euros?",
+}).then((result) => console.log(result.text))
+  .catch(console.error);
