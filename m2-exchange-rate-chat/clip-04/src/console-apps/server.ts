f0a91bbeac2aebb08d685cb0600b3a83db0a5d0c
pkellner <peter@peterkellner.net>
2025-02-05 17:35:35 -0800

    update

---
+++ m2-exchange-rate-chat/clip-04/src/console-apps/server.ts
@@ -0,0 +1,13 @@
+import { generateText } from "ai";
+import { openai } from "@ai-sdk/openai";
+
+async function main(): Promise<void> {
+  const result = await generateText({
+    model: openai("gpt-4"),
+    prompt: "What is move valuable, 50 Euros or 50 Dollars?",
+  });
+
+  console.log(result.text); // Print the complete response
+}
+
+main().catch(console.error);
