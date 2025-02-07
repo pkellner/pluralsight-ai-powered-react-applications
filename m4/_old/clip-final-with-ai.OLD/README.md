1e2052cf2a2fda28829adccc654d24a1167d2532
pkellner <peter@peterkellner.net>
2025-02-06 16:20:01 -0800

    update

--- m4/_old/clip-final-with-ai.OLD/README.md
+++
@@ -1,1 +0,0 @@
-# React AI Course
--- m4/_old/clip-final-with-no-ai/src/app/ai/summarize-content.ts
+++
@@ -1,4 +0,0 @@
-export async function summarizeContent(body: string): Promise<string> {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  return `Email Length: ${body.length} characters`;
-}
